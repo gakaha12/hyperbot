aa933ca7ab84e91c438d96ee67996614bc4e3fb3
gakaha12 <103883535+gakaha12@users.noreply.github.com>
2022-04-17 17:56:45 +0530

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export TOKEN=
-export BOT_NAME=
-export USE_stockfish14m=true
+export TOKEN=lip_7X6gFZvlhobBs6a8HTG1
+export BOT_NAME=YoVABot
+export USE_LC0=false
 export ENGINE_MOVE_OVERHEAD=5000
 
 node server.js
